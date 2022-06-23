7c0abf49365e93d04f39cb1149f33bc5e1196c96
R0m@in <91207256+rfontainepro@users.noreply.github.com>
2022-06-23 16:20:05 +0200

    Correction d'un bug sous Mobile + Firefox
    <meta name="viewport" content="width=device-width, maximum-scale=1.0">

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, maximum-scale=1.0">
+
+		<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+
+
 		<title>Formulaire de Contact (AJAX)</title>
 		<link rel="stylesheet" href="./assets/css/main.css" />
 
